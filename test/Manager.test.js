c6c30da2f5f2a74bc8004937edd6bd50139a1511
ssingh <sandy.boon@gmail.com>
2020-04-18 19:33:49 -0400

    Added Manager class and corresponsding tests

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,22 +1,37 @@
 const Manager = require('../lib/Manager');
 const Employee = require('../lib/Employee');
 
-describe.skip('Manager', () => {
+describe('Manager', () => {
   test('Can set office number via constructor argument', () => {
     const testValue = 100;
-    const e = new Manager('Foo', 1, 'test@test.com', testValue);
+    const e = new Manager({
+      name: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      officeNumber: testValue,
+    });
     expect(e.officeNumber).toBe(testValue);
   });
 
   test('getRole() should return "Manager"', () => {
     const testValue = 'Manager';
-    const e = new Manager('Foo', 1, 'test@test.com', 100);
+    const e = new Manager({
+      name: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      officeNumber: testValue,
+    });
     expect(e.getRole()).toBe(testValue);
   });
 
   test('Can get office number via getOffice()', () => {
     const testValue = 100;
-    const e = new Manager('Foo', 1, 'test@test.com', testValue);
+    const e = new Manager({
+      name: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      officeNumber: testValue,
+    });
     expect(e.getOfficeNumber()).toBe(testValue);
   });
 });
