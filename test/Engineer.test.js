eb44a76eb819859bf9042bc50a523ad26c62becb
ssingh <sandy.boon@gmail.com>
2020-04-18 21:44:29 -0400

    Added Engineer and Inrern Class and corresponding tests

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,21 +1,36 @@
 const Engineer = require('../lib/Engineer');
 
-describe.skip('Enginer', () => {
+describe('Enginer', () => {
   test('Can set GitHUb account via constructor', () => {
     const testValue = 'GitHubUser';
-    const e = new Engineer('Foo', 1, 'test@test.com', testValue);
+    const e = new Engineer({
+      name: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      github: testValue,
+    });
     expect(e.github).toBe(testValue);
   });
 
   test('getRole() should return "Engineer"', () => {
     const testValue = 'Engineer';
-    const e = new Engineer('Foo', 1, 'test@test.com', 'GitHubUser');
+    const e = new Engineer({
+      name: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      github: 'GitHubUser',
+    });
     expect(e.getRole()).toBe(testValue);
   });
 
   test('Can get GitHub username via getGithub()', () => {
     const testValue = 'GitHubUser';
-    const e = new Engineer('Foo', 1, 'test@test.com', testValue);
+    const e = new Engineer({
+      name: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      github: testValue,
+    });
     expect(e.getGithub()).toBe(testValue);
   });
 });
--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,20 +1,35 @@
 const Intern = require('../lib/Intern');
-describe.skip('Intern', () => {
+describe('Intern', () => {
   test('Can set school via constructor', () => {
     const testValue = 'UCLA';
-    const e = new Intern('Foo', 1, 'test@test.com', testValue);
+    const e = new Intern({
+      namd: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      school: testValue,
+    });
     expect(e.school).toBe(testValue);
   });
 
   test('getRole() should return "Intern"', () => {
     const testValue = 'Intern';
-    const e = new Intern('Foo', 1, 'test@test.com', 'UCLA');
+    const e = new Intern({
+      namd: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      school: 'UCLA',
+    });
     expect(e.getRole()).toBe(testValue);
   });
 
   test('Can get school via getSchool()', () => {
     const testValue = 'UCLA';
-    const e = new Intern('Foo', 1, 'test@test.com', testValue);
+    const e = new Intern({
+      namd: 'Foo',
+      id: 1,
+      email: 'test@test.com',
+      school: testValue,
+    });
     expect(e.getSchool()).toBe(testValue);
   });
 });
