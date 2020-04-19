62128734ec084518a91ed935a4695461263729b5
ssingh <sandy.boon@gmail.com>
2020-04-19 16:52:22 -0400

    Collect all employee information. Create employee objects and save

--- app.js
+++ app.js
@@ -1,18 +1,79 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const IdGenerator = require('./lib/IdGenerator');
+const questions = require('./lib/questions');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+const render = require('./lib/htmlRenderer');
 
-
+const idProducer = new IdGenerator();
+const employeesInformation = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+async function init() {
+  console.log('************Welcome to the Team Portal Builder**************');
+  console.log('Please answer a few questions to build a portal for your team');
+  try {
+    const managerInfo = await getManagerInfo();
+    employeesInformation.push(managerInfo);
+    do {
+      const employeeInfo = await gatherEmployeeInfo();
+      employeesInformation.push(employeeInfo);
+    } while (await doesUserWantToAddMoreEmployees());
+    //map the collected information to appropriate employee objects
+    const employees = employeesInformation.map((empInfo) => {
+      return getEmployeeObject(addIdToProvidedInfo(empInfo));
+    });
+    console.log(employees);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getManagerInfo() {
+  const managerInfo = await inquirer.prompt(questions.getManagerInfo);
+  return addEmployeeType(managerInfo, 'Manager');
+}
+
+async function gatherEmployeeInfo() {
+  const { employeeType } = await inquirer.prompt(questions.getEmployeeType);
+  const employeeInfo = await inquirer.prompt(
+    getQuestionForEmployeeType(employeeType)
+  );
+  return addEmployeeType(employeeInfo, employeeType);
+}
+
+function addEmployeeType(employeeInfo, employeeType) {
+  employeeInfo.employeeType = employeeType;
+  return employeeInfo;
+}
+
+async function doesUserWantToAddMoreEmployees() {
+  const answer = await inquirer.prompt(questions.addMoreEmployeeQues);
+  return answer.addMore === 'Yes';
+}
+
+function getQuestionForEmployeeType(employeeType) {
+  switch (employeeType.toLowerCase()) {
+    case 'engineer':
+      return questions.getEngineerInfo;
+      break;
+    case 'intern':
+      return questions.getInternInfo;
+      break;
+    case 'manager':
+      return questions.getManagerInfo;
+      break;
+    default:
+      throw new Error('Invalid Employee type!');
+  }
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +94,34 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+function getEmployeeObject(employeeInfo) {
+  let employeeType = employeeInfo.employeeType;
+  let employeeObj = null;
+  switch (employeeType.toLowerCase()) {
+    case 'manager':
+      employeeObj = new Manager(employeeInfo);
+      break;
+
+    case 'engineer':
+      employeeObj = new Engineer(employeeInfo);
+      break;
+
+    case 'intern':
+      employeeObj = new Intern(employeeInfo);
+      break;
+
+    default:
+      break;
+  }
+  console.log(employeeObj);
+  console.log(employeeObj.getRole());
+  return employeeObj;
+}
+
+function addIdToProvidedInfo(employeeObj) {
+  employeeObj.id = idProducer.getNextId();
+  return employeeObj;
+}
+
+init();
